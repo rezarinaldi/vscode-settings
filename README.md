eb7c9468cf5da55a24f2e5a3c97b79d52971eb4a
rezarinaldi <rez4rinaldi@gmail.com>
2021-04-28 20:15:10 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# 📗 My VSCode Settings
+
+I am using: [VSCode Insiders](https://code.visualstudio.com/insiders/).
+
+![Preview](img/preview.png)
+
+### 🍀 Theme
+
+I am using theme: [Icy Kiss & Deep Jungle Theme](https://marketplace.visualstudio.com/items?itemName=stelo.icykiss).
+
+### 🧶 Extensions
+
+![Extensions](img/extensions.png)
+
+### 🧩 Settings file
+
+Go to
+
+- [settings.json](settings.ts)
+
+(it has .ts extension in order to properly display comments on github)
---
+++ settings.ts
@@ -0,0 +1,69 @@
+{
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+
+  // for prettier configuration
+  "[javascript]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+
+  // format code on save
+  "editor.formatOnSave": true,
+
+  // update import on file move
+  "javascript.updateImportsOnFileMove.enabled": "always",
+
+  // other settings here
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  "editor.fontFamily": "Jetbrains Mono",
+  "editor.fontLigatures": true,
+  "todo-tree.tree.showScanModeButton": false,
+  "workbench.iconTheme": "city-lights-icons-vsc",
+  "git.autofetch": true,
+  "git.enableSmartCommit": true,
+  "workbench.colorTheme": "Deep Jungle",
+  "editor.minimap.enabled": false,
+  "workbench.sideBar.location": "right",
+  "intelephense.diagnostics.undefinedTypes": false,
+  "intelephense.diagnostics.undefinedFunctions": false,
+  "intelephense.diagnostics.undefinedConstants": false,
+  "intelephense.diagnostics.undefinedClassConstants": false,
+  "intelephense.diagnostics.undefinedProperties": false,
+  "intelephense.diagnostics.undefinedMethods": false,
+  "intelephense.diagnostics.undefinedVariables": false,
+  "emmet.triggerExpansionOnTab": true,
+  "blade.format.enable": true,
+  "[blade]": {
+    "editor.autoClosingBrackets": "always"
+  },
+  "[css]": {
+    "editor.defaultFormatter": "michelemelluso.code-beautifier"
+  },
+  "[svelte]": {
+    "editor.defaultFormatter": "svelte.svelte-vscode"
+  },
+  "[csharp]": {
+    "editor.defaultFormatter": "ms-dotnettools.csharp"
+  },
+  "[dart]": {
+    "editor.defaultFormatter": "Dart-Code.dart-code"
+  },
+  "clang-format.executable": "C:/Users/Reza/.vscode-insiders/extensions/ms-vscode.cpptools-1.3.1/LLVM/bin/clang-format",
+  "[cpp]": {
+    "editor.defaultFormatter": "xaver.clang-format"
+  },
+  "omnisharp.enableRoslynAnalyzers": true,
+  "dart.previewFlutterUiGuidesCustomTracking": true,
+  "dart.previewFlutterUiGuides": true,
+  "workbench.editorAssociations": [
+    {
+      "viewType": "jupyter-notebook",
+      "filenamePattern": "*.ipynb"
+    }
+  ],
+  "[python]": {
+    "editor.defaultFormatter": null
+  },
+  "python.formatting.provider": "black"
+}
